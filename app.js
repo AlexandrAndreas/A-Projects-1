1673aeb9643353fff761fabad34ed95a8cde7d8a
AlexandrAndreas <38665039+AlexandrAndreas@users.noreply.github.com>
2018-04-25 20:56:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-var http = require("http");
-http.createServer(function(request, response) {
-    response.end("Hello NodeJS!");
-}).listen(3000, "127.0.0.1", function() {
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-});
+//var os = require("os");
+//var greeting1 = require("./greeting.js");
+//var User = require("./user.js");
+
+//var myName = new User("Александр", 46);
+
+//myName.sayHi();
+
+//var userName = os.userInfo().username;
+
+//console.log(`Дата запроса: ${greeting.date}`);
+//console.log(greeting.getMessage(userName));
+
+/*console.log(`Hello ${greeting1.name}`);
+
+var greeting2 = require("./greeting.js");
+greeting2.name = "Bob";
+
+console.log(`Hello ${greeting2.name}`);
+
+console.log(`Hello ${greeting1.name}`);*/
+
+var welcom = require("./welcom");
+
+//welcom.default.getMoningMessage();
+//welcom.default.getEveningMessage();
+
+welcom.getEveningMessage();
+welcom.getMoningMessage();
